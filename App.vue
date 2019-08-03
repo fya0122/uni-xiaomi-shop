ac55666f3c6ce3d29a59f2e25f1d09f18523f490
fya0122 <18730930122@163.com>
2019-08-03 22:24:49 +0800

    '引入了4个库'

--- App.vue
+++ App.vue
@@ -1,3 +1,13 @@
 <script>
 export default {};
 </script>
+<style>
+/* 官方uni.css */
+@import './common/uni.css';
+/* 第三方动画库 */
+@import './common/animate.css';
+/* 自定义图标库 */
+@import './common/icon.css';
+/* 公共样式 */
+@import './common/common.css';
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,22 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view>
+		<button type="primary" @click="gotochuxian">出现吧</button>
+		<view v-if="show" class="animated fadeIn fast">hello.</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
+export default {
+	data() {
+		return {
+			show: false
+		};
+	},
+	onLoad() {},
+	methods: {
+		gotochuxian() {
+			this.show = true;
 		}
 	}
+};
 </script>
-
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50upx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
-</style>
