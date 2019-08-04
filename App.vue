ff10d416029783c1c214c9bbcd6dfaf6765007a6
fya0122 <18730930122@163.com>
2019-08-04 10:31:47 +0800

    '更改了主色调'

--- App.vue
+++ App.vue
@@ -10,4 +10,25 @@ export default {};
 @import './common/icon.css';
 /* 公共样式 */
 @import './common/common.css';
+page {
+	height: 100%;
+	font-size: 28upx;
+	line-height: 1.8;
+}
+image {
+	width: 100%;
+}
+/* 主色调 */
+.main-text-color {
+	color: #fd6801;
+}
+.main-bg-color {
+	background: #fd6801;
+}
+.main-border-color {
+	border-color: #fd6801;
+}
+.main-bg-hover-color {
+	background: rgba(253, 104, 1, 0.85);
+}
 </style>
