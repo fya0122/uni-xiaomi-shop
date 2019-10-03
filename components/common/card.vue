a74ad823afbbe9a18cc3b8e03e0c6097b00a3e1e
fya0122 <18730930122@163.com>
2019-10-03 14:37:49 +0800

    '搜索列表ok'

--- components/common/card.vue
+++ components/common/card.vue
@@ -1,12 +1,12 @@
 <template>
 	<view class="card">
 		<!-- 标题 -->
-		<view v-if="showhead" class="p-2 border-bottom main-border-color">
+		<view v-if="showhead" class="p-2 main-border-color" :class="getHeadClass">
 			<slot name="title">
 				<text v-if="headTitle" class="font-md font-weight">{{ headTitle }}</text>
 			</slot>
 		</view>
-		<view>
+		<view :class="getBodyClass">
 			<image v-if="bodyCover" :src="bodyCover" mode="widthFix"></image>
 			<slot></slot>
 		</view>
@@ -25,7 +25,25 @@ export default {
 		showhead: {
 			type: Boolean,
 			default: true
-		}
-	}
-};
+		},
+    headBorderBottom:{
+      type: Boolean,
+      default: true
+    },
+    bodyPadding:{
+      type: Boolean,
+      default: false
+    }
+	},
+  computed: {
+    getHeadClass() {
+      let BorderBottom = this.headBorderBottom ? 'border-bottom':''
+      return `${BorderBottom}`
+    },
+    getBodyClass(){
+      let padding = this.bodyPadding ? 'p-2' : ''
+      return `${padding}`
+    }
+  }
+}
 </script>
--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,11 +1,56 @@
 <template>
   <div>
-    搜索页
+    <card headTitle="热门搜索" bodyCover="../../static/images/demo/search-banner.png"></card>
+    <!-- 多色按钮 -->
+    <view class="px-1 mb-2">
+      <color-tag v-for="item of hot" :key="item.name" :item="item"></color-tag>
+    </view>
+    <!-- 常用分类 -->
+    <card :bodyPadding="false" :headBorderBottom="false" headTitle="常用分类">
+      <color-tag :color="false" v-for="item of cate" :key="item.name" :item="item"></color-tag>
+    </card>
+    <!-- 分割线 -->
+    <divider/>
+    <!-- 搜搜记录 -->
+    <card headTitle="搜索记录">
+      <uni-list-item title="小米" :showArrow="false"></uni-list-item>
+      <uni-list-item title="小米2" :showArrow="false"></uni-list-item>
+    </card>
   </div>
 </template>
 
 <script>
+import card from '@/components/common/card.vue'
+import colorTag from '@/components/search/color-tag.vue'
+import divider from '@/components/common/divider.vue'
+import uniListItem from '@/components/uni-ui/uni-list-item/uni-list-item.vue'
+export default {
+  data() {
+    return {
+      hot: [
+        { name: '领券中心' },
+        { name: 'Redmi K20' },
+        { name: 'RedmiBook 14' },
+        { name: '智能积木 越野四驱车' },
+        { name: '手环 腕带' }
+      ],
+      cate: [
+        { name: '耳机' },
+        { name: '手机' },
+        { name: '音箱' },
+        { name: '手表' },
+        { name: '配件' },
+        { name: '网关/传感器' },
+        { name: '健康' },
+        { name: '酷玩' }
+      ]
+    }
+  },
+  components: {
+    card,
+    colorTag,
+    divider,
+    uniListItem
+  },
+}
 </script>
-
-<style>
-</style>
